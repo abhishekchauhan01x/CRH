e085d9dd12ce383fcd3004d54e439eeea17eec78
Abhishek Chauhan <abhishekchauhan01x@gmail.com>
2025-11-01 20:12:15 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
     'https://crh-ruby.vercel.app',
-    'https://crh-rvfg.vercel.app/'
+    'https://crh-rvfg.vercel.app'
 ];
 
 app.use(cors({
